194d46a37dff17eeed72f93d87cc7dfd50b976d4
Ling <765874050@qq.com>
2021-07-30 22:04:43 +0800

    first commit

---
+++ Module01/src/bean/test.java
@@ -0,0 +1,19 @@
+package bean;
+
+/**
+ * @TIME : 2021/7/14 23:13
+ * @Author : Ling
+ * @File : test
+ * @Software : IntelliJ IDEA
+ **/
+
+//test
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Ling");
+        for (int i = 0; i < 100; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+
+}
