76080ce4786e0839872e5f0393e0f78acac84114
acetime01 <88141212+acetime01@users.noreply.github.com>
2021-07-31 23:03:41 +0800

    pull commit

--- src/com/atguigu/java/HelloWorld.java
+++ src/com/atguigu/java/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
         System.out.println("Here is hot-fix branch");
         System.out.println("master test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 
 }
