0c92647daccf7c1e6ef4c97c71592dec5b40d3d9
Ling <765874050@qq.com>
2021-07-31 15:37:15 +0800

    push commit

--- src/com/atguigu/java/HelloWorld.java
+++ src/com/atguigu/java/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
         System.out.println("Ling");
         System.out.println("Here is hot-fix branch");
         System.out.println("master test");
+        System.out.println("push test");
     }
 
 }
